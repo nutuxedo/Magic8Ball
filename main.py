6873226cc55b35d15184c4d1eb35b24de9b8ee31
Rafael Jose <141491411+nutuxedo@users.noreply.github.com>
2024-11-25 18:34:52 +0800

    Added the functionality to end the program after confirming - rearranged the codebase

--- main.py
+++ main.py
@@ -4,77 +4,19 @@ import tkinter as tk
 from tkinter import messagebox
 from tkinter import PhotoImage
 import pygame
+import sys
 
-def exit_con():
-    con_window = tk.Tk()
-    con_window.title("Exit")
-    con_window.resizable(False,False)
-    #con_window.geometry("150x150")
-    ex_com = tk.Label(
-        con_window,
-        text = "Are you sure you want to exit?",
-        font=("Cantarell", 12, "bold"),
-        bg="white",
-        height="2",
-        width="30"
-    )
-    ex_yes = tk.Button(
-        con_window,
-        text = "Yes",
-        bg="sky blue",
-        command = con_window.destroy
-    )
-
-    ex_no = tk.Button(
-        con_window,
-        text = "No",
-        bg="sky blue",
-        command = con_window.destroy
-    )
-    ex_com.grid(row =0, column= 0, columnspan= 2)
-    ex_yes.grid(row=1, column= 0, pady =10)
-    ex_no.grid(row=1, column=1, pady=10)
-
-def about_win():
-    #img = PhotoImage(file="imgs/Magic_eight_ball.png")
-    about_window = tk.Tk()
-    about_window.geometry("150x80")
-    about_window.title("About")
-    about_window.resizable(False, False)
-    #about_img = tk.Label(
-        #about_window,
-        #image = img
-    #)
-    about_msg = tk.Label(
-        about_window,
-        text="8 ball funsies",
-        font=("Cantarell", 12, "bold"),
-        bg="white",
-        width="15"
-    )
-    version_num = tk.Label(
-        about_window,
-        text="by nutuxedo - version 0",
-        font=("Cantarell", 8),
-        bg="white",
-        width="25"
-    )
-    new_button = tk.Button(
-        about_window,
-        text = "Exit",
-        bg="sky blue",
-        command = about_window.destroy
-    )
-    #about_img.pack()
-    about_msg.pack()
-    version_num.pack()
-    new_button.pack()
+# main 8 ball content here
+class EightBall:
+    def __init__(self):
+        print("You have shaken the ball!")
 
 # main window code here
 def mainwindow():
     root = tk.Tk()
     root.title("8 Ball funsies")
     root.geometry("250x160")
+    root.resizable(False,False)
     maincontent = tk.Label(
         root,
         text="8 ball content here",
@@ -120,11 +62,71 @@ def mainwindow():
     about_btn.grid(row = 3, column = 0, ipadx = 10, pady = 5)
     exit_btn.grid(row = 3, column = 1, ipadx = 10, pady = 5)
     root.mainloop()
+    return root
+def exit_con():
+    con_window = tk.Toplevel(mainwindow)
+    con_window.title("Exit")
+    con_window.resizable(False,False)
+    #con_window.geometry("150x150")
+    ex_com = tk.Label(
+        con_window,
+        text = "Are you sure you want to exit?",
+        font=("Cantarell", 12, "bold"),
+        bg="white",
+        height="2",
+        width="30"
+    )
+    ex_yes = tk.Button(
+        con_window,
+        text = "Yes",
+        bg="sky blue",
+        command = sys.exit
+    )
 
-# main 8 ball content here
-class EightBall:
-    def __init__(self):
-        print("You have shaken the ball!")
+    ex_no = tk.Button(
+        con_window,
+        text = "No",
+        bg="sky blue",
+        command = con_window.destroy
+    )
+    ex_com.grid(row =0, column= 0, columnspan= 2)
+    ex_yes.grid(row=1, column= 0, pady =10)
+    ex_no.grid(row=1, column=1, pady=10)
+
+def about_win():
+    #img = PhotoImage(file="imgs/Magic_eight_ball.png")
+    about_window = tk.Toplevel(mainwindow)
+    about_window.geometry("150x80")
+    about_window.title("About")
+    about_window.resizable(False, False)
+    #about_img = tk.Label(
+        #about_window,
+        #image = img
+    #)
+    about_msg = tk.Label(
+        about_window,
+        text="8 ball funsies",
+        font=("Cantarell", 12, "bold"),
+        bg="white",
+        width="15"
+    )
+    version_num = tk.Label(
+        about_window,
+        text="by nutuxedo - version 0",
+        font=("Cantarell", 8),
+        bg="white",
+        width="25"
+    )
+    new_button = tk.Button(
+        about_window,
+        text = "Exit",
+        bg="sky blue",
+        command = about_window.destroy
+    )
+    #about_img.pack()
+    about_msg.pack()
+    version_num.pack()
+    new_button.pack()
 
 if __name__ == "__main__":
     mainwindow()
